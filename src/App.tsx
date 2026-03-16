084b3f3852b48b56cf026f1b02987c9b4638dae1
Natt och Dag, Rickard <rickard.natt-och-dag@seb.se>
2026-03-16 09:43:03 +0100

    chore: simplify reproduction even more

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
 import React from "react";
 import { Pressable, Text, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, UnistylesRuntime } from "react-native-unistyles";
 import "./theme";
 import { createStaticNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Animated from "react-native-reanimated";
 
 function First() {
-	const [checked, setChecked] = React.useState(false);
-
 	return (
-		<View style={{ flex: 1 }}>
-			<View style={styles.container()}>
-				<View style={{ rowGap: 24 }}>
-					<Animated.View
-						style={[
-							styles.checkbox(checked),
-							{
-								transitionProperty: ["backgroundColor"],
-								transitionDuration: 1000,
-							},
-						]}
-					/>
-					<Animated.View style={styles.checkbox(checked)} />
-				</View>
-				<View style={{ flexDirection: "row", columnGap: 8 }}>
-					<Pressable style={styles.button} onPress={() => setChecked(!checked)}>
-						<Text>Check box</Text>
-					</Pressable>
-				</View>
-			</View>
+		<View style={styles.container()}>
+			<Pressable style={styles.button}>
+				<Text>Themed button</Text>
+			</Pressable>
 		</View>
 	);
 }
@@ -66,17 +45,11 @@ const RootStack = createNativeStackNavigator({
 		First: {
 			screen: First,
 			options: ({ navigation }) => ({
-				headerRight: () => {
-					return (
-						<Pressable
-							onPress={() => {
-								navigation.navigate("Settings");
-							}}
-						>
-							<Text>Settings</Text>
-						</Pressable>
-					);
-				},
+				headerRight: () => (
+					<Pressable onPress={() => navigation.navigate("Settings")}>
+						<Text>Settings</Text>
+					</Pressable>
+				),
 			}),
 		},
 		Settings,
@@ -86,21 +59,10 @@ const RootStack = createNativeStackNavigator({
 const Navigation = createStaticNavigation(RootStack);
 
 function App() {
-	return (
-		<SafeAreaProvider>
-			<GestureHandlerRootView style={{ flex: 1 }}>
-				<Navigation />
-			</GestureHandlerRootView>
-		</SafeAreaProvider>
-	);
+	return <Navigation />;
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
-	checkbox: (checked: boolean) => ({
-		backgroundColor: checked ? theme.colors.green : theme.colors.red,
-		width: 40,
-		height: 40,
-	}),
 	container: () => {
 		console.log(rt.themeName);
 
@@ -110,13 +72,9 @@ const styles = StyleSheet.create((theme, rt) => ({
 			flex: 1,
 		};
 	},
-	card: {
-		backgroundColor: "#333",
-		padding: 24,
-	},
 	button: {
 		backgroundColor: theme.colors.primary,
-		padding: 24,
+		padding: 23,
 	},
 }));
 
